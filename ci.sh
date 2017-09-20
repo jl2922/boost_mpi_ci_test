a1579d6a5a7520af6f5640cf367f5600cad4c3bf
Junhao Li <streaver91@gmail.com>
2017-09-20 07:17:03 -0400

    cache

--- ci.sh
+++ ci.sh
@@ -2,19 +2,22 @@ test -n $CC && unset CC
 test -n $CXX && unset CXX
 
 # Get OpenMPI.
-if [ -f "openmpi-3.0.0/bin/mpirun" ]; then
+if [ -f "deps/openmpi-3.0.0/bin/mpirun" ]; then
 	echo "Using cached OpenMPI"
 else
 	echo "Downloading OpenMPI Source"
+  mkdir -p downloads
+  cd downloads
 	wget https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.bz2
 	tar xjf openmpi-3.0.0.tar.bz2
 	echo "Configuring and building OpenMPI"
 	cd openmpi-3.0.0
-	./configure --prefix=$TRAVIS_BUILD_DIR/openmpi-3.0.0 CC=$C_C CXX=$CXX_C &> configure.log
+	./configure --prefix=$TRAVIS_BUILD_DIR/deps/openmpi-3.0.0 CC=$C_C CXX=$CXX_C &> configure.log
 	make -j 4 &> make.log
 	make install &> install.log
-	cd ..
+	cd ../../
 fi
-export PATH=$TRAVIS_BUILD_DIR/openmpi-3.0.0/bin:$PATH
+export PATH=$TRAVIS_BUILD_DIR/deps/openmpi-3.0.0/bin:$PATH
 
+mpic++ --version
 make -j
--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <cstdio>
+#include <unordered_map>
 
-int main()
-{
+int main() {
   printf("hello world\n");
+  std::unordered_map<int, int> m;
+  m[1] = 2;
   return 0;
 }

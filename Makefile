2ff206d920a04019667ecb331619cb90a982e630
Junhao Li <streaver91@gmail.com>
2017-09-20 07:37:37 -0400

    protobuf

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CXX := mpic++
 CXXFLAGS := -std=c++17 -Wall -Wextra -fopenmp -O3 -g
+LDLIBS := -L $TRAVIS_BUILD_DIR/deps/protobuf-3.4.1/lib -lprotobuf
 TEST_EXE := test.x
 
 LOCAL_MAKEFILE := local.mk
@@ -11,4 +12,4 @@ all: $(TEST_EXE)
 	./$(TEST_EXE)
 
 $(TEST_EXE): main.cc
-	$(CXX) $(CXXFLAGS) main.cc -o $(TEST_EXE)
+	$(CXX) $(CXXFLAGS) main.cc -o $(TEST_EXE) -lprotobuf
--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@ test -n $CC && unset CC
 test -n $CXX && unset CXX
 
 # Get OpenMPI.
-if [ -f "deps/openmpi-3.0.0/bin/mpirun" ]; then
+if [ -f "deps/openmpi-3.0.0/bin/mpic++" ] && [ -f "deps/openmpi-3.0.0/bin/mpic++" ]; then
 	echo "Using cached OpenMPI"
 else
 	echo "Downloading OpenMPI Source"
@@ -12,12 +12,34 @@ else
 	tar xjf openmpi-3.0.0.tar.bz2
 	echo "Configuring and building OpenMPI"
 	cd openmpi-3.0.0
+  mkdir -p $TRAVIS_BUILD_DIR/deps/openmpi-3.0.0
 	./configure --prefix=$TRAVIS_BUILD_DIR/deps/openmpi-3.0.0 CC=$C_C CXX=$CXX_C &> configure.log
 	make -j 4 &> make.log
 	make install &> install.log
 	cd ../../
 fi
 export PATH=$TRAVIS_BUILD_DIR/deps/openmpi-3.0.0/bin:$PATH
+export LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/deps/openmpi-3.0.0/lib:$LD_LIBRARY_PATH
+
+# Get Protocol Buffers.
+if [ -f "deps/protobuf-3.4.1/bin/protoc" ]; then
+	echo "Using cached Protocol Buffers"
+else
+	echo "Downloading Protocol Buffers"
+  mkdir -p downloads
+  cd downloads
+	wget https://github.com/google/protobuf/releases/download/v3.4.1/protobuf-cpp-3.4.1.tar.gz
+	tar xzf protobuf-cpp-3.4.1.tar.gz
+	echo "Configuring and building Protocol Buffers"
+	cd protobuf-3.4.1
+  mkdir -p $TRAVIS_BUILD_DIR/deps/protobuf-3.4.1
+	./configure --prefix=$TRAVIS_BUILD_DIR/deps/protobuf-3.4.1 CC=$C_C CXX=$CXX_C &> configure.log
+	make -j 4 &> make.log
+	make install &> install.log
+	cd ../../
+fi
+export PATH=$TRAVIS_BUILD_DIR/deps/protobuf-3.4.1/bin:$PATH
+export LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/deps/protobuf-3.4.1/lib:$LD_LIBRARY_PATH
 
 mpic++ --version
 make -j
